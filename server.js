0dee9fb82355e7fa780b06eb40d3378e6cca1740
Nicole Franklin <Ncfranklin14@gmail.com>
2022-05-06 14:23:05 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const allRoutes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// const { add models } = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const sess = {
+    secret: "Super secret secret",
+    cookie: {
+      maxAge: 2 * 60 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  app.use(session(sess));
+  // Static directory
+  app.use(express.static('public'));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use("/", allRoutes);
+
+sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
