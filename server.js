e3016f0a183a586e65de149bbe0b633053b6a099
ester kim <kimester1996@gmail.com>
2022-05-09 11:46:20 -0800

    connected to local host

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const allRoutes = require("./controllers");
+// const allRoutes = require("./controllers");
 const session = require("express-session");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -32,7 +32,7 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use("/", allRoutes);
+// app.use("/", allRoutes);
 
 sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
