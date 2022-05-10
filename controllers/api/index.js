4162de42bd473856073af1b33728b4b616ca0f8f
Kalif P <kalifpurce@gmail.com>
2022-05-10 15:19:22 -0700

    Routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
+const express =require('express');
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-// const blogRoutes = require('./blogRoutes');
-
 router.use('/users', userRoutes);
-// router.use('/blogs', blogRoutes);
+
+const blogRoutes = require('./blogRoutes');
+router.use('/blogs', blogRoutes);
+
+
 
 module.exports = router;
